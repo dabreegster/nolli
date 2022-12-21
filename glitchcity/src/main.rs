708cb3c136bc2ebff0ae0a04ecbc6262518b8235
Dustin Carlino <dabreegster@gmail.com>
2022-12-21 19:45:29 +0000

    Initial 3D attempt. Not happy with the camera

--- glitchcity/src/main.rs
+++ glitchcity/src/main.rs
@@ -1,18 +1,21 @@
 use anyhow::Result;
 use bevy::prelude::{
-    default, App, Assets, Camera2dBundle, Color, ColorMaterial, Commands,
-    DefaultPlugins, Mesh, ResMut
+    default, shape, App, Assets, Camera3dBundle, Color, Commands, DefaultPlugins, Mesh, PbrBundle,
+    PointLight, PointLightBundle, ResMut, StandardMaterial, Transform, Vec3,
 };
-use bevy::sprite::MaterialMesh2dBundle;
 use bevy_inspector_egui::WorldInspectorPlugin;
-use bevy_pancam::{PanCam, PanCamPlugin};
+use smooth_bevy_cameras::{
+    controllers::fps::{FpsCameraBundle, FpsCameraController, FpsCameraPlugin},
+    LookTransformPlugin,
+};
 
 mod load_geo;
 
 fn main() -> Result<()> {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(PanCamPlugin::default())
+        .add_plugin(LookTransformPlugin)
+        .add_plugin(FpsCameraPlugin::default())
         .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup)
         .run();
@@ -23,7 +26,7 @@ fn main() -> Result<()> {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let mut args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
@@ -33,12 +36,42 @@ fn setup(
 
     let (buildings, _) = load_geo::load_buildings(&path).unwrap();
 
-    commands.spawn(MaterialMesh2dBundle {
+    /*commands.spawn(MaterialMesh2dBundle {
         mesh: meshes.add(load_geo::polygons_to_mesh(buildings)).into(),
         material: materials
-            .add(ColorMaterial::from(Color::hex("601865").unwrap()))
+            .add(Color::hex("601865").unwrap())
             .into(),
         ..default()
+    });*/
+
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
     });
-    commands.spawn((Camera2dBundle::default(), PanCam::default()));
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands
+        .spawn(Camera3dBundle::default())
+        .insert(FpsCameraBundle::new(
+            FpsCameraController {
+                translate_sensitivity: 15.0,
+                ..default()
+            },
+            // eye
+            Vec3::new(-2.0, 5.0, 5.0),
+            // target
+            Vec3::new(0., 0., 0.),
+        ));
 }
